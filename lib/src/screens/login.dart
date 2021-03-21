6d127b14f7b9dea2659dbf39e2f2876611432358
Akshat Sharma <akshatsharma992001@gmail.com>
2021-03-21 12:40:39 +0530

    Pixel overflow fixed

--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -15,144 +15,153 @@ class _LoginState extends State<Login> {
   final password = TextEditingController();
 
   @override
+  void dispose() {
+    emailId.dispose();
+    password.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return (Scaffold(
-      backgroundColor: Colors.purple[100],
+      // backgroundColor: Colors.purple[100],
       body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.only(top: 70),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(
-                'Login',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 35,
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.only(top: 70),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 35,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Form(
-                key: _formkey,
-                child: Column(
+                SizedBox(
+                  height: 40,
+                ),
+                Form(
+                  key: _formkey,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 15, right: 15),
+                        child: TextFormField(
+                          controller: emailId,
+                          cursorColor: Colors.purple,
+                          autofocus: true,
+                          onChanged: (val) {
+                            user.email = val;
+                          },
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your email id';
+                            }
+                            return null;
+                          },
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                          decoration: InputDecoration(
+                            labelText: 'Email ID',
+                            labelStyle: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                            errorStyle: TextStyle(fontSize: 13),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.purple),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.purple),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 15, right: 15),
+                        child: TextFormField(
+                          controller: password,
+                          obscureText: true,
+                          cursorColor: Colors.purple,
+                          onChanged: (val) {
+                            user.password = val;
+                          },
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            return null;
+                          },
+                          style: TextStyle(fontSize: 20.0),
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                            errorStyle: TextStyle(fontSize: 13),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.purple),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.purple),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                FlatButton(
+                  onPressed: () {
+                    if (_formkey.currentState.validate()) {
+                      print(user.email);
+                      print(user.password);
+                      emailId.clear();
+                      password.clear();
+                    }
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  color: Colors.purple[400],
+                  height: 37,
+                  splashColor: Colors.purple[500],
+                ),
+                SizedBox(height: 25),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 15, right: 15),
-                      child: TextFormField(
-                        controller: emailId,
-                        cursorColor: Colors.purple,
-                        autofocus: true,
-                        onChanged: (val) {
-                          user.email = val;
-                        },
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter your email id';
-                          }
-                          return null;
-                        },
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(width: 6),
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.popAndPushNamed(context, '/signup');
+                      },
+                      child: Text(
+                        'Sign Up!',
                         style: TextStyle(
-                          fontSize: 20,
-                        ),
-                        decoration: InputDecoration(
-                          labelText: 'Email ID',
-                          labelStyle: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
-                          ),
-                          errorStyle: TextStyle(fontSize: 13),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.purple),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.purple),
-                          ),
-                        ),
+                            fontSize: 16, fontWeight: FontWeight.w800),
                       ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 15, right: 15),
-                      child: TextFormField(
-                        controller: password,
-                        obscureText: true,
-                        cursorColor: Colors.purple,
-                        onChanged: (val) {
-                          user.password = val;
-                        },
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter your password';
-                          }
-                          return null;
-                        },
-                        style: TextStyle(fontSize: 20.0),
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          labelStyle: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
-                          ),
-                          errorStyle: TextStyle(fontSize: 13),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.purple),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.purple),
-                          ),
-                        ),
-                      ),
+                      splashColor: Colors.purple[200],
                     ),
                   ],
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              FlatButton(
-                onPressed: () {
-                  if (_formkey.currentState.validate()) {
-                    print(user.email);
-                    print(user.password);
-                    emailId.clear();
-                    password.clear();
-                  }
-                },
-                child: Text(
-                  'Login',
-                  style: TextStyle(fontSize: 18),
-                ),
-                color: Colors.purple[400],
-                height: 37,
-                splashColor: Colors.purple[500],
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an account?",
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(width: 6),
-                  FlatButton(
-                    onPressed: () {
-                      Navigator.popAndPushNamed(context, '/signup');
-                    },
-                    child: Text(
-                      'Sign Up!',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.w800),
-                    ),
-                    splashColor: Colors.purple[200],
-                  ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/src/screens/signup.dart
+++ lib/src/screens/signup.dart
@@ -17,184 +17,194 @@ class _SignUpState extends State<SignUp> {
   final confirmpassword = TextEditingController();
 
   @override
+  void dispose() {
+    emailId.dispose();
+    password.dispose();
+    confirmpassword.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return (Scaffold(
-      backgroundColor: Colors.purple[100],
+      // backgroundColor: Colors.blue,
       body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.only(top: 29),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(
-                'SignUp',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 35,
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.only(top: 70),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'SignUp',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 35,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Form(
-                key: _form,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 15, right: 15),
-                      child: TextFormField(
-                        controller: emailId,
-                        cursorColor: Colors.purple,
-                        autofocus: true,
-                        onChanged: (val) {
-                          user.email = val;
-                        },
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter your email id';
-                          }
-                          return null;
-                        },
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                        decoration: InputDecoration(
-                          labelText: 'Email ID',
-                          labelStyle: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
+                SizedBox(
+                  height: 40,
+                ),
+                Form(
+                  key: _form,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 15, right: 15),
+                        child: TextFormField(
+                          controller: emailId,
+                          cursorColor: Colors.purple,
+                          autofocus: true,
+                          onChanged: (val) {
+                            user.email = val;
+                          },
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your email id';
+                            }
+                            return null;
+                          },
+                          style: TextStyle(
+                            fontSize: 20,
                           ),
-                          errorStyle: TextStyle(fontSize: 13),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.purple),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.purple),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 4,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 15, right: 15),
-                      child: TextFormField(
-                        controller: password,
-                        obscureText: true,
-                        cursorColor: Colors.purple,
-                        onChanged: (val) {
-                          user.password = val;
-                        },
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter your password';
-                          }
-                          return null;
-                        },
-                        style: TextStyle(fontSize: 20.0),
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          labelStyle: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
-                          ),
-                          errorStyle: TextStyle(fontSize: 13),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.purple),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.purple),
+                          decoration: InputDecoration(
+                            labelText: 'Email ID',
+                            labelStyle: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                            errorStyle: TextStyle(fontSize: 13),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.purple),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.purple),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 4,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 15, right: 15),
-                      child: TextFormField(
-                        cursorColor: Colors.purple,
-                        onChanged: (val) {
-                          user.confirmPassword = val;
-                        },
-                        obscureText: true,
-                        controller: confirmpassword,
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please confirm your password';
-                          } else if (user.password != value) {
-                            return 'Both passwords do not match';
-                          }
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 15, right: 15),
+                        child: TextFormField(
+                          controller: password,
+                          obscureText: true,
+                          cursorColor: Colors.purple,
+                          onChanged: (val) {
+                            user.password = val;
+                          },
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            return null;
+                          },
+                          style: TextStyle(fontSize: 20.0),
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                            errorStyle: TextStyle(fontSize: 13),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.purple),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.purple),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 15, right: 15),
+                        child: TextFormField(
+                          cursorColor: Colors.purple,
+                          onChanged: (val) {
+                            user.confirmPassword = val;
+                          },
+                          obscureText: true,
+                          controller: confirmpassword,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please confirm your password';
+                            } else if (user.password != value) {
+                              return 'Both passwords do not match';
+                            }
 
-                          return null;
-                        },
-                        style: TextStyle(fontSize: 20.0),
-                        decoration: InputDecoration(
-                          labelText: 'Confirm Password',
-                          labelStyle: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
-                          ),
-                          errorStyle: TextStyle(fontSize: 13),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.purple),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.purple),
+                            return null;
+                          },
+                          style: TextStyle(fontSize: 20.0),
+                          decoration: InputDecoration(
+                            labelText: 'Confirm Password',
+                            labelStyle: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                            errorStyle: TextStyle(fontSize: 13),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.purple),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.purple),
+                            ),
                           ),
                         ),
                       ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                FlatButton(
+                  onPressed: () {
+                    if (_form.currentState.validate()) {
+                      print(user.email);
+                      print(user.password);
+                      print(user.confirmPassword);
+                      emailId.clear();
+                      password.clear();
+                      confirmpassword.clear();
+                    }
+                  },
+                  child: Text(
+                    'SignUp',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  color: Colors.purple[400],
+                  height: 37,
+                  splashColor: Colors.purple[500],
+                ),
+                SizedBox(height: 25),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Already have an account?",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(width: 6),
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.popAndPushNamed(context, '/');
+                      },
+                      child: Text(
+                        'Login!',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w800),
+                      ),
+                      splashColor: Colors.purple[200],
                     ),
                   ],
                 ),
-              ),
-              SizedBox(
-                height: 29,
-              ),
-              FlatButton(
-                onPressed: () {
-                  if (_form.currentState.validate()) {
-                    print(user.email);
-                    print(user.password);
-                    print(user.confirmPassword);
-                    emailId.clear();
-                    password.clear();
-                    confirmpassword.clear();
-                  }
-                },
-                child: Text(
-                  'SignUp',
-                  style: TextStyle(fontSize: 18),
-                ),
-                color: Colors.purple[400],
-                height: 37,
-                splashColor: Colors.purple[500],
-              ),
-              SizedBox(height: 5),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Already have an account?",
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(width: 6),
-                  FlatButton(
-                    onPressed: () {
-                      Navigator.popAndPushNamed(context, '/');
-                    },
-                    child: Text(
-                      'Login!',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.w800),
-                    ),
-                    splashColor: Colors.purple[200],
-                  ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
