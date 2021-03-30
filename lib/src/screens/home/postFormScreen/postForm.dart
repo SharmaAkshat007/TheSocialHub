601d29003ae9957ea478ebf73b1a1ddae8c14fd2
Akshat Sharma <akshatsharma992001@gmail.com>
2021-03-30 23:16:01 +0530

    upload ui done

--- lib/src/screens/home/postFormScreen/postForm.dart
+++ lib/src/screens/home/postFormScreen/postForm.dart
@@ -13,40 +13,95 @@ class _PostFormState extends State<PostForm> {
 
   @override
   Widget build(BuildContext context) {
+    final _deviceHeight = MediaQuery.of(context).size.height;
+    final _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.only(top: 10),
-        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-          IconButton(
-            icon: Icon(Icons.camera_alt),
-            onPressed: () async {
-              File image = await PickImage().captureImage(true);
+        child: _image != null
+            ? Image.file(
+                _image,
+                fit: BoxFit.contain,
+                width: _deviceWidth,
+                height: _deviceHeight,
+              )
+            : Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Container(
+                  width: _deviceWidth,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        splashColor: Colors.purple[100],
+                        icon: Icon(
+                          Icons.add,
+                          size: 28,
+                        ),
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text("Camera or Photos"),
+                                  actions: [
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.camera_alt,
+                                        color: Colors.purple,
+                                      ),
+                                      onPressed: () async {
+                                        File image = await PickImage()
+                                            .captureImage(true);
 
-              if (image != null) {
-                setState(() {
-                  _image = image;
-                });
-              }
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.drive_folder_upload),
-            onPressed: () async {
-              dynamic image = await PickImage().captureImage(false);
+                                        if (image != null) {
+                                          setState(() {
+                                            _image = image;
+                                          });
+                                          Navigator.pop(context);
+                                        }
+                                      },
+                                    ),
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.drive_folder_upload,
+                                        color: Colors.purple,
+                                      ),
+                                      onPressed: () async {
+                                        dynamic image = await PickImage()
+                                            .captureImage(false);
 
-              if (image != null) {
-                setState(() {
-                  _image = image;
-                });
-              }
-            },
-          ),
-          _image == null
-              ? Center(
-                  child: Text('Image not found'),
-                )
-              : Image.file(_image, height: 300, width: 300),
-        ]),
+                                        if (image != null) {
+                                          setState(() {
+                                            _image = image;
+                                          });
+                                          Navigator.pop(context);
+                                        }
+                                      },
+                                    ),
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.cancel,
+                                        color: Colors.purple,
+                                      ),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                  ],
+                                );
+                              });
+                        },
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        'Upload an Image',
+                        style: TextStyle(fontSize: 20, color: Colors.purple),
+                      ),
+                    ],
+                  ),
+                ),
+              ]),
       ),
     );
   }
